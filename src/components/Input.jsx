a5b63e212cf8af97e23d31f5a3a3919a1d819faa
Yash Patil <yashpatil182000@gmail.com>
2025-04-30 16:34:21 +0530

    Input component disble prop added

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
-function Input({ name, label, placeholder, type, value, onChange }) {
+function Input({
+  name,
+  label,
+  placeholder,
+  type,
+  value,
+  onChange,
+  disabled = false,
+}) {
   return (
     <>
       <div className="w-full flex flex-col gap-2">
@@ -12,8 +20,11 @@ function Input({ name, label, placeholder, type, value, onChange }) {
           name={name}
           value={value}
           placeholder={placeholder}
-          className="bg-secondaryLight rounded-lg px-5 py-1 text-gray-600 border-1 border-gray-300 focus:border-primary focus:ring-primary outline-none"
+          className={`bg-secondaryLight rounded-lg px-5 py-1 text-gray-600 border-1 border-gray-300 focus:border-primary focus:ring-primary outline-none ${
+            disabled && "opacity-60"
+          }`}
           onChange={onChange}
+          disabled={disabled}
         />
       </div>
     </>
